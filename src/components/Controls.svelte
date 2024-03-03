377352f9cfcdfd042a8629ec2c05e3ae7effd8ae
Michael Riley <michaelriley87@gmail.com>
2024-03-04 10:18:19 +1100

    initial commit

---
+++ src/components/Controls.svelte
@@ -0,0 +1,13 @@
+<script>
+    let isPlaying = false;
+
+    function togglePlay() {
+        isPlaying = !isPlaying;
+    }
+</script>
+
+<svelte>
+    <button on:click={togglePlay}>
+        {isPlaying ? "Stop" : "Play"}
+    </button>
+</svelte>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import headerImage from '../assets/header.png';
+    import Controls from '../components/Controls.svelte';
+</script>
+
+<main>
+    <img src={headerImage} alt="beat originator" />
+    <Controls />
+</main>
